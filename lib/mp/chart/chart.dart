8393e3e48b557da2200fd12735008d9539e1acd1
mohammed.abdullah <mohammed.abdullah@warbabank.com>
2021-05-25 11:23:55 +0200

    clean up

--- lib/mp/chart/chart.dart
+++ lib/mp/chart/chart.dart
@@ -48,8 +48,10 @@ abstract class ChartState<T extends Chart> extends State<T> {
     String fileName = DateTime.now().toIso8601String();
     String path = '$directory/$fileName.png';
     _screenshotController.capture(path: path, pixelRatio: 3.0).then((imgFile) {
+
       ImageGallerySaver.saveImage(Uint8List.fromList(imgFile.readAsBytesSync()))
-          .then((value) {
+          .then((dynamic value) {
+
         imgFile.delete();
       });
       isCapturing = false;
